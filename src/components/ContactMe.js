b1a591b7d73483aac879f6d64f4561a46261580b
almostTaklu <ach.ganga@outlook.com>
2024-05-31 00:56:07 -0400

    Update ContactMe section and ReadMe

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,18 +1,33 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import '../styles/styles.css';
 
-const ContactMe = () => (
-  <div className="contact-me-section container" id="contact-me">
-    <form action="./public/contact.php" method="post">
-      <input type="text" name="name" placeholder="Your Name" required />
-      <input type="email" name="email" placeholder="Your Email" required />
-      <textarea name="message" placeholder="Your Message" required></textarea>
-      <button type="submit">Send</button>
-    </form>
-    <div className="email-info">
-      <p>Email: your-email@example.com</p>
+const Contact = () => {
+  return (
+    <div className="contact-me-section" id="contact">
+      <Container>
+        <h2 className="contact-me-title">Contact Me</h2>
+        <Row>
+          <Col sm={12} md={6} className="form-container">
+            <form action="contact.php" method="post">
+              <h4>Send me a Message</h4>
+              <input type="text" name="name" placeholder="Your Name" required />
+              <input type="email" name="email" placeholder="Your Email" required />
+              <textarea name="message" rows="5" placeholder="Your Message" required></textarea>
+              <button type="submit">Send Message</button>
+            </form>
+          </Col>
+          <Col sm={12} md={5} className="email-info">
+            <h4>Get in Touch</h4>
+            <p>I am actively seeking opportunities as a software or web developer. If you have any openings or just want to connect, please reach out to me at <a href="mailto:ach.ganga@outlook.com">ach.ganga@outlook.com</a>.</p>
+          </Col>
+        </Row>
+        <Row className="d-block d-md-none">
+          <div className="line"></div>
+        </Row>
+      </Container>
     </div>
-  </div>
-);
+  );
+};
 
-export default ContactMe;
+export default Contact;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { FaHeart, FaReact } from 'react-icons/fa';
+import { FaCode, FaHeart, FaReact } from 'react-icons/fa';
 import '../styles/styles.css';
 
 const Footer = () => (
   <footer>
     <p>
-      &lt;/&gt; with <FaHeart /> by Ganga Acharya using <FaReact />
+      <FaCode/>with <FaHeart /> by Ganga Acharya using <FaReact />
     </p>
   </footer>
 );
