b8780edfb2d4e373f25d04235ddd62e3e46856b1
almostTaklu <ach.ganga@outlook.com>
2024-06-12 17:17:31 -0400

    Bug fixes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './About.css';
-import '../../css/styles.css'; 
+import '../../css/styles.css';
+import profileImage from '../../assets/images/profile.jpg';
+import resume from '../../assets/resume/Ganga_s_Resume.pdf';
 
 const About = () => {
   return (
@@ -8,14 +10,14 @@ const About = () => {
       <div className="container">
         <h2 className="about-title">About Me</h2>
         <div className="about-content">
-          <img src="https://via.placeholder.com/300" alt="Profile" className="about-image" />
+          <img src={profileImage} alt="Profile" className="about-image" />
           <div className="about-text">
             <p>
               Hello, I'm Ganga Acharya,
               a passionate web developer with a solid foundation in computer science. I recently graduated from Millersville University with a Bachelor of Science in Computer Science. My journey through college has equipped me with skills in Java, Python, C/C++, SQL (Postgres), and JavaScript, along with hands-on experience using tools like Git, Gradle, and VS Code. I’ve worked on exciting projects, including a Python-based UML Editor and a dynamic, responsive blogger website using the MEAN stack. These projects have sharpened my ability to create user-friendly interfaces and integrate robust functionalities.
               <br></br>
             </p>
-            <a href="path/to/your-resume.pdf" className="resume-button button" download>Download Resume</a> 
+            <a href={resume} className="resume-button button" download>Download Resume</a> 
           </div>
         </div>
       </div>
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -20,7 +20,7 @@ const Contact = () => {
             <h3 className="contact-info-title">Get in Touch</h3>
             <p>I am currently looking for a web/software developer job. I am passionate about creating efficient and scalable applications and am eager to bring my skills to a dynamic team. Feel free to reach out to me via email.</p>
             <div className="contact-social-icons">
-              <a href="mailto:example@example.com"><i className="fas fa-envelope"></i></a>
+              <a href="mailto:ach.ganga@outlook.com"><i className="fas fa-envelope"></i></a>
               <a href="https://linkedin.com/in/acharyagan" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a>
               <a href="https://github.com/almostTaklu" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a>
               <a href="https://x.com/aganga02" target="_blank" rel="noopener noreferrer"><i className="fab fa-x-twitter"></i></a>
