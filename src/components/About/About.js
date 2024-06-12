d0e199d70c37cb883d331fd64fbb8fc61be27a82
almostTaklu <ach.ganga@outlook.com>
2024-06-12 13:34:28 -0400

    Update colors in About Me section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './About.css';
-import '../../css/styles.css'; // Assuming styles.css is in the src directory
+import '../../css/styles.css'; 
 
 const About = () => {
   return (
@@ -15,7 +15,7 @@ const About = () => {
               a passionate web developer with a solid foundation in computer science. I recently graduated from Millersville University with a Bachelor of Science in Computer Science. My journey through college has equipped me with skills in Java, Python, C/C++, SQL (Postgres), and JavaScript, along with hands-on experience using tools like Git, Gradle, and VS Code. I’ve worked on exciting projects, including a Python-based UML Editor and a dynamic, responsive blogger website using the MEAN stack. These projects have sharpened my ability to create user-friendly interfaces and integrate robust functionalities.
               <br></br>
             </p>
-            <a href="path/to/your-resume.pdf" className="resume-button button" download>Download Resume</a> {/* Added 'button' class */}
+            <a href="path/to/your-resume.pdf" className="resume-button button" download>Download Resume</a> 
           </div>
         </div>
       </div>
